0cda289beb6b8398fc51fdf2ecc4606d3973421e
Sadman Shoumik <101177954+SadmanShoumik@users.noreply.github.com>
2023-10-03 10:56:35 +0600

    Add files via upload

---
+++ Basic Algorithms/ReverseArray with Recursion/Program.cs
@@ -0,0 +1,26 @@
+﻿
+void rev(int s, int e, params object[] obj)
+{
+    if(s>=e)
+        return;
+
+    var temp = obj[0];
+
+    temp = obj[s];
+    obj[s] = obj[e];
+    obj[e] = temp;
+
+    rev(s + 1, e - 1, obj);
+
+}
+
+string s = Console.ReadLine();
+
+var obj = s.Split(" ");
+
+rev(0, obj.Length-1, obj);
+
+for(int i = 0; i < obj.Length; i++ )
+{
+    Console.Write(obj[i] + " ");
+}
