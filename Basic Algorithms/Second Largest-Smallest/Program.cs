405db90e1e3d583505cdde684152c2a9e419c49c
Sadman Shoumik <101177954+SadmanShoumik@users.noreply.github.com>
2023-10-03 10:58:24 +0600

    Add files via upload

---
+++ Basic Algorithms/Second Largest-Smallest/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = new int[] {1, 2, 3, 4, 5, 10, 23, 2, 53, 100, 34, 36, 98, 96, 23, 56, 84, 53, 34};
+
+int i, max=0, max2=0, min=1000, min2=1000;
+
+for(i=0; i<arr.Length; i++)
+{
+    if (arr[i]>max)
+    {
+        max2 = max;
+        max = arr[i];
+    }
+    else if (arr[i]>max2)
+    {
+        max2 = arr[i];
+    }
+
+    if (arr[i] < min)
+    {
+        min2 = min;
+        min = arr[i];
+    }
+    else if (arr[i] < min2)
+    {
+        min2 = arr[i];
+    }
+}
+
+Console.WriteLine(max2);
+Console.WriteLine(min2);
