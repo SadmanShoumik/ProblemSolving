d24cc37216f5f393ce5ca0a4628e7d2f8de8b8f6
Sadman Shoumik <101177954+SadmanShoumik@users.noreply.github.com>
2023-10-03 10:57:31 +0600

    Add files via upload

---
+++ Basic Algorithms/Rotate Array by K Element Optimal/Program.cs
@@ -0,0 +1,22 @@
+﻿void rev(int[] arr, int s, int e)
+{
+    while(s<e)
+    {
+        //Neat
+        (arr[s], arr[e]) = (arr[e], arr[s]);
+        s++;
+        e--;
+    }
+}
+
+int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int k = int.Parse(Console.ReadLine());
+k = k % arr.Length;
+
+rev(arr, arr.Length - k, arr.Length - 1);
+rev(arr, 0, arr.Length - k - 1);
+rev(arr, 0, arr.Length - 1);
+
+for(int i=0; i<arr.Length; i++)
+    Console.Write(arr[i]+" ");
