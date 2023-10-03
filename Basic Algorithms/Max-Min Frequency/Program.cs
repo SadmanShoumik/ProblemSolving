cf3b4c0332155a88174547982cb28ac87a30b588
Sadman Shoumik <101177954+SadmanShoumik@users.noreply.github.com>
2023-10-03 10:55:01 +0600

    Add files via upload

---
+++ Basic Algorithms/Max-Min Frequency/Program.cs
@@ -0,0 +1,22 @@
+﻿string s = Console.ReadLine();
+var obj = s.Split(" ");
+
+int i;
+int[] arr = new int[obj.Length];
+Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+
+for (i=0; i<obj.Length; i++)
+{
+    arr[i] = int.Parse(obj[i]);
+
+    if (dictionary.ContainsKey(arr[i]))
+        dictionary[arr[i]]++;
+    else
+        dictionary.Add(arr[i], 1);
+}
+
+foreach(var item in dictionary)
+{
+    Console.WriteLine(item.Key + " --- " + item.Value);
+}
---
+++ Basic Algorithms/Palindrome with Recursion/Program.cs
@@ -0,0 +1,21 @@
+﻿void IsPal(string str, int s, int e)
+{
+    if (s >= e)
+    {
+        Console.WriteLine("YES");
+        return;
+    }
+
+    if (str[s] != str[e])
+    {
+        Console.WriteLine("NO");
+        return;
+    }
+    else
+        IsPal(str, s + 1, e - 1);
+}
+
+string s = Console.ReadLine();
+s = s.ToLower();
+
+IsPal(s, 0, s.Length-1);
